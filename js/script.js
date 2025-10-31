74918bb3aa2e32b62b0dec287f948d5afeae0574
FKAYSR <kejserfreja@gmail.com>
2025-10-31 08:41:09 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,35 @@
 "use strict";
 
 const quotes = [
-    {"quotation"},
-    {"quotations"},
-    {"quotationers"},
-    {"QUOTATION"},
-    {"QuOtAtIoN"}
+    "quotation",
+    "quotations",
+    "quotationers",
+    "QUOTATION",
+    "QuOtAtIoN"
 ];
-const randomIndex = quotes.length;
-const randomQuote = document.getElementById("randomQuote");
 
 function getRandomQuote() {
     if (quotes.length === 0) {
         return 'No quotes available';
     }
-    Math.random();
-    Math.floor();
 }
+
+const randomIndex = Math.floor(Math.random()*quotes.length);
+return quotes [randomIndex]
+
+function displayRandomQuote() {
+    const randomQuote = getRandomQuote()
+    const quoteElement = document.getElementById("displayQuoteBtn");
+    quoteElement.textContent = randomQuote;
+}
+
+const  button = const document.getElementById("displayQuoteBtn");
+button.addEventListener(`click`, displayRandomQuote);
+
+
+/* // #region 
+const randomIndex = quotes.length;
+const randomQuote = document.getElementById("randomQuote");
 
 return quotes[randomIndex];
 
@@ -26,4 +39,4 @@ function displayRandomQuote() {
 }
 
 const document.getElementById("displayQuoteBtn").addEventListener("click", () =>);
-
+//#endregion */
